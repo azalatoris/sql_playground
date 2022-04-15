3a3b93ab96318cd6f96a38bc74da1b3aaccd9acd
vaporyhumo <roanvilina@gmail.com>
2022-04-15 03:21:57 -0400

    Migrate and add seeds

---
+++ db/schema.rb
@@ -0,0 +1,108 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_051436) do
+  create_table "bus_models", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "buses", force: :cascade do |t|
+    t.integer "bus_model_id", null: false
+    t.string "plate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_model_id"], name: "index_buses_on_bus_model_id"
+  end
+
+  create_table "route_stops", force: :cascade do |t|
+    t.integer "route_id", null: false
+    t.integer "stop_id", null: false
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["route_id"], name: "index_route_stops_on_route_id"
+    t.index ["stop_id"], name: "index_route_stops_on_stop_id"
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "trip_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_sales_on_trip_id"
+    t.index ["user_id"], name: "index_sales_on_user_id"
+  end
+
+  create_table "seats", force: :cascade do |t|
+    t.integer "bus_model_id", null: false
+    t.string "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_model_id"], name: "index_seats_on_bus_model_id"
+  end
+
+  create_table "stops", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "sale_id", null: false
+    t.integer "seat_id", null: false
+    t.integer "user_id", null: false
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sale_id"], name: "index_tickets_on_sale_id"
+    t.index ["seat_id"], name: "index_tickets_on_seat_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "route_id", null: false
+    t.integer "bus_id", null: false
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_id"], name: "index_trips_on_bus_id"
+    t.index ["route_id"], name: "index_trips_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "buses", "bus_models"
+  add_foreign_key "route_stops", "routes"
+  add_foreign_key "route_stops", "stops"
+  add_foreign_key "sales", "trips"
+  add_foreign_key "sales", "users"
+  add_foreign_key "seats", "bus_models"
+  add_foreign_key "tickets", "sales"
+  add_foreign_key "tickets", "seats"
+  add_foreign_key "tickets", "users"
+  add_foreign_key "trips", "buses"
+  add_foreign_key "trips", "routes"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,85 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or create!d alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create!([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create!(name: "Luke", movie: movies.first)
+
+
+# User
+# BusModel
+# Seat
+# Bus
+# Stop
+# Route
+# RouteStop
+#
+
+
+10.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email
+
+  User.create!(name: name, email: email)
+end
+users = User.all
+
+10.times do
+  BusModel.create!(name: Faker::Cannabis.brand)
+end
+
+BusModel.find_each do |bus_model|
+  30.times do |i|
+    label = i
+    Seat.create!(label: label, bus_model: bus_model)
+  end
+end
+
+BusModel.find_each do |bus_model|
+  5.times do
+    Bus.create!(bus_model: bus_model, plate: Faker::Alphanumeric.alphanumeric(number: 10))
+  end
+end
+buses = Bus.all
+
+30.times do
+  name = Faker::Name.name
+
+  Stop.create!(name: name)
+end
+
+10.times do
+  name = Faker::Name.name
+
+  Route.create!(name: name)
+end
+
+Route.find_each do |route|
+  stops = Stop.all.sample(10)
+
+  10.times do |order|
+    RouteStop.create!(route: route, stop: stops[order], order: order + 1)
+  end
+end
+
+Route.find_each do |route|
+  10.times do
+    Trip.create!(route: route, date: Time.now, bus: buses.sample)
+  end
+end
+trips = Trip.all
+
+100.times do
+  user = users.sample
+  trip = trips.sample
+
+  Sale.create!(user: user, trip: trip)
+end
+sales = Sale.all
+
+1000.times do
+  sale = sales.sample
+  seat = sale.trip.bus.bus_model.seats.sample
+  Ticket.create!(sale: sale, seat: seat, passenger: users.sample)
+end
